cbe5588ff5e3011298fd4de8be26c1206475c87d
Borsodi Dávid <corten.mgs@gmail.com>
2020-05-22 14:52:40 +0200

    Changed whitelisted cors origin

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://corteno.github.io/reddit-meme-fixer/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
